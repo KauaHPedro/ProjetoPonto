3f127b5e4e4cd4d46ae6d1dae8afd92060da3f78
KauaHPedro <kauahcpedro@hotmail.com>
2025-06-23 23:32:22 -0300

    criacao da classe database

---
+++ src/config/database.php
@@ -0,0 +1,24 @@
+<?php
+
+class Database {
+    public static function getConnection() {
+        $envPath = realpath(dirname(__FILE__) . '/../env.ini');
+        $env = parse_ini_file($envPath);
+
+        $host =  $env['host'];
+        $port =  $env['port'];
+        $username =  $env['username'];
+        $password =  $env['password'];
+        $database =  $env['database'];
+
+        try {
+            $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$database", $username, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Erro: " . $e->getMessage());
+        }
+
+        return $pdo;
+
+    }
+}
