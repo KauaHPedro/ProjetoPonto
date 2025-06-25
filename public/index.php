7a6de6a1709e63b9b5dd2c653ae37dd2bc95d8d8
KauaHPedro <kauahcpedro@hotmail.com>
2025-06-24 22:43:53 -0300

    setando arquivo config e apontando no index

--- public/index.php
+++ public/index.php
@@ -1,4 +1,32 @@
-<h1>Ok</h1>
+<?php
 
-Repo criado para armazenar o projeto final do curso de PHP da Cod3r, uma aplicação de registro de ponto
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+
+require_once(dirname(__FILE__, 2) . '/src/config/config.php');
+
+$query = "SELECT * FROM users";
+$users = Database::getResultFromQuery($query);
+
+foreach ($users as $user) {
+    echo $user['name'] . '<br>';
+}
+
+//$delete = "DELETE FROM users WHERE id IN (:id, :id2, :id3)";
+//$params = [
+//    ':id' => 6,
+//    ':id2' => 11,
+//    ':id3' => 12
+//];
+//
+//$rows = Database::executeNonQuery($delete, $params);
+//
+//if ($rows > 0) {
+//    echo "Success";
+//    echo "<br>";
+//    echo "Rows affected: " . $rows . "<br>";
+//} else {
+//    echo "Error";
+//}
