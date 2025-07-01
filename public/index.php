7d9616d4fd3090292759a711f4b32edc3a6112bd
KauaHPedro <kauahcpedro@hotmail.com>
2025-06-30 23:40:01 -0300

    adicionando tratamento para index.php tambem

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 
 $uri = urldecode($_SERVER['REQUEST_URI']);
 
-if ($uri === "/" || $uri === "") {
+if ($uri === "/" || $uri === "" || $uri === "/index.php") {
     $uri = "/login.php";
 }
 
