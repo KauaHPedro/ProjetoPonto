c69631aa0393ed11dbaecd30a40e01f0eb907c5a
KauaHPedro <kauahcpedro@hotmail.com>
2025-07-09 14:28:46 -0300

    ajustes na tela de registro de ponto

--- src/views/daily_records.php
+++ src/views/daily_records.php
@@ -1,6 +1,33 @@
 <main class="content">
-    <h1>Olá!</h1>
-    <h1>
-        <?= $viewData["user"] ?>
-    </h1>
+    <div class="content-title mb-4">
+        <i class="icon icofont-check-alt mr-2"></i>
+        <div>
+            <h1>Registrar Ponto</h1>
+            <h2>Mantenha o seu Ponto consistente!</h2>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <h3>9 de julho de 2025</h3>
+            <p class="mb-0">Batimentos efetuados hoje</p>
+        </div>
+        <div class="card-body">
+            <div class="d-flex m-5 justify-content-around">
+                <span class="record">Entrada 1: ----</span>
+                <span class="record">Saída 1: ----</span>
+            </div>
+            <div class="d-flex m-5 justify-content-around">
+                <span class="record">Entrada 2: ----</span>
+                <span class="record">Saída 2: ----</span>
+            </div>
+        </div>
+        <div class="card-footer d-flex justify-content-center">
+            <a href="???" class="btn btn-success btn-lg">
+                <i class="icofont-check mr-1">
+                    Bater o Ponto
+                </i>
+            </a>
+        </div>
+    </div>
 </main>
